9c6f6635c613e1a32ee5f30c2c84c34a8ad11f7e
Simon Larsen <simonhffh@gmail.com>
2019-01-28 16:25:17 -0800

    Added new script for building gene lists from UCSC web server

--- data-raw/genelists.R
+++ data-raw/genelists.R
@@ -1,13 +1,36 @@
-library(usethis)
+library(readr)
+library(dplyr)
+library(GenomicRanges)
 
-files <- c(
-  hg18 = "glist-hg18",
-  hg19 = "glist-hg19",
-  hg38 = "glist-hg38"
+ref <- c(
+  "hg18", "hg19", "hg38", # human
+  "mm8", "mm9", "mm10", #mouse
+  "rn5", "rn6" # rat
 )
 
-genes <- lapply(files, function(file) {
-  read.table(file, header=FALSE, col.names=c("chr","start","end","name"), stringsAsFactors=FALSE)
+REFGENE_URL <- "http://hgdownload.cse.ucsc.edu/goldenPath/%s/database/refGene.txt.gz"
+
+genes <- lapply(ref, function(refname) {
+  message(refname)
+  D <- read_tsv(sprintf(REFGENE_URL, refname), col_names=FALSE) %>%
+    select(3,5,6,13) %>%
+    magrittr::set_colnames(c("chr","start","end","name")) %>%
+    mutate(chr = gsub("^chr", "", chr))
+  
+  # merge overlapping gene ranges in same chromosome
+  ranges <- makeGRangesFromDataFrame(D, keep.extra.columns=TRUE, ignore.strand=TRUE) %>%
+    split(D$name) %>%
+    reduce %>%
+    unlist
+  
+  glist <- data.frame(
+    chr=as.character(seqnames(ranges)),
+    start=start(ranges),
+    end=end(ranges),
+    name=names(ranges),
+    stringsAsFactors=FALSE, row.names=NULL
+  )
 })
+names(genes) <- ref
 
 usethis::use_data(genes, overwrite=TRUE, internal=TRUE)
