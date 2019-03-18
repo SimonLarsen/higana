bb57c7cd8e187018f4ea80df27bf0a354478a61e
Simon Larsen <simonhffh@gmail.com>
2019-03-18 16:38:51 +0100

    Option to specify test method in test_terms

--- R/assoc.R
+++ R/assoc.R
@@ -4,49 +4,53 @@
 #' @param covars A data frame containing covariates.
 #' @param pc Named list of PCs from terms. Computed with \code{\link{compute_term_pcs}}.
 #' @param family Error distribution and link function to be used in the model. See \code{\link{glm.fit}} for details.
+#' @param Test statistic for model comparison. One of "Chisq", "LRT", "Rao", "F" or "Cp". See \code{\link{stat.anova}}.
 #' @param terms Character vector of terms to test. Defaults to all terms.
 #' @param max_pcs Maximum number of PCs to use per term.
 #' @param num_threads Number of threads.
 #' @return A list with elements:
 #'   \describe{
-#'     \item{\code{reference}}{Reference model.}
+#'     \item{\code{test}}{Test objects for each term.}
 #'     \item{\code{coef}}{Coefficients from each term model.}
-#'     \item{\code{tests}}{Test objects for each term.}
-#'     \item{\code{pvalue}}{p-values for each term.}
 #'   }
 #' @importFrom pbapply pblapply
 #' @importFrom pbmcapply pbmclapply
 #' @importFrom fastmatch "%fin%"
 #' @export
-test_terms <- function(formula, covars, pc, family=binomial("logit"), terms=NULL, max_pcs=Inf, num_threads=1) {
+test_terms <- function(formula, covars, pc, family=binomial("logit"), test=NULL, terms=NULL, max_pcs=Inf, num_threads=1) {
   if(class(formula) != "formula") stop("formula is not a formula.")
   if(!("data.frame" %in% class(covars))) stop("covars is not a data frame.")
   if(any(grepl("TERM[0-9]+", colnames(covars)))) stop("Covariates named \"TERM[n]\" where [n] is a number are not allowed.")
   if(any(sapply(pc, is.null))) stop("Principal component vector contains NULL values.")
 
+  if(is.null(test)) {
+    if(family$family %in% c("binomial","poisson")) test <- "Chisq"
+    else if(family$family %in% c("gaussian","quasibinomial","quasipoisson")) test <- "F"
+    else stop("Don't know which test statistic to use for family \"", family$family, "\"")
+  }
   if(is.null(terms)) terms <- names(pc)
 
   reference.model <- glm(formula, covars, family=family, na.action=na.omit)
 
   out <- pbmclapply(pc[terms], function(term) {
-    x <- term$u
+    if(length(term$d > 1)) x <- term$u %*% diag(term$d)
+    else x <- term$u * term$d
+
     if(ncol(x) > max_pcs) x <- x[,seq_len(max_pcs), drop=FALSE]
+
     colnames(x) <- paste0("TERM", seq_len(ncol(x)))
     D <- cbind(covars, x, stringsAsFactors=FALSE, row.names=NULL)
 
     formula.term <- update(formula, paste0("~ . + ", paste0(colnames(x), collapse="+")))
     fit <- glm(formula.term, D, family=family, na.action=na.omit, model=FALSE)
-    test <- anova(reference.model, fit, test="LRT")
-    list(test=test, coef=coefficients(summary(fit)))
+    t <- anova(reference.model, fit, test="LRT")
+
+    list(test=t, coef=coefficients(summary(fit)))
   }, mc.cores=num_threads)
 
-  pvalues <- sapply(out, function(t) t$test$`Pr(>Chi)`[2])
-
   list(
-    reference = reference.model,
     coef=lapply(out, `[[`, "coef"),
-    tests=lapply(out, `[[`, "test"),
-    pvalue=pvalues
+    test=lapply(out, `[[`, "test")
   )
 }
 
@@ -57,24 +61,30 @@ test_terms <- function(formula, covars, pc, family=binomial("logit"), terms=NULL
 #' @param pc Named list of PCs from terms. Computed with \code{\link{compute_term_pcs}}.
 #' @param o An \code{ontology} object.
 #' @param family Error distribution and link function to be used in the model. See \code{\link{glm.fit}} for details.
+#' @param Test statistic for model comparison. One of "Chisq", "LRT", "Rao", "F" or "Cp". See \code{\link{stat.anova}}.
 #' @param terms Character vector of terms to test. Defaults to all terms.
 #' @param max_pcs Maximum number of PCs to use per term.
 #' @param num_threads Number of threads.
 #' @return A list with elements:
 #'   \describe{
-#'     \item{\code{tests}}{Test objects for each term.}
-#'     \item{\code{pvalue}}{p-values for each term.}
+#'     \item{\code{test}}{Test objects for each term.}
+#'     \item{\code{coef}}{Coefficients from each term model.}
 #'   }
 #' @importFrom fastmatch "%fin%"
 #' @importFrom pbmcapply pbmclapply
 #' @export
-test_terms_children <- function(formula, covars, pc, o, family=binomial("logit"), terms=NULL, max_pcs=Inf, num_threads=1) {
+test_terms_children <- function(formula, covars, pc, o, family=binomial("logit"), test=NULL, terms=NULL, max_pcs=Inf, num_threads=1) {
   if(class(formula) != "formula") stop("formula is not a formula.")
   if(!("data.frame" %in% class(covars))) stop("covars is not a data frame.")
   if(any(grepl("TERM[0-9]+", colnames(covars)))) stop("Covariates named \"TERM[n]\" where [n] is a number are not allowed.")
   if(any(grepl("CHILD[0-9]+", colnames(covars)))) stop("Covariates named \"CHILD[n]\" where [n] is a number are not allowed.")
   if(any(sapply(pc, is.null))) stop("Principal component vector contains NULL values.")
 
+  if(is.null(test)) {
+    if(family$family %in% c("binomial","poisson")) test <- "Chisq"
+    else if(family$family %in% c("gaussian","quasibinomial","quasipoisson")) test <- "F"
+    else stop("Don't know which test statistic to use for family \"", family$family, "\"")
+  }
   if(is.null(terms)) terms <- names(pc)
   terms <- intersect(terms, go$id[lengths(go$children) > 0])
 
@@ -106,15 +116,15 @@ test_terms_children <- function(formula, covars, pc, o, family=binomial("logit")
     formula.term <-  update(formula.child, paste0("~ . + ", paste0(colnames(x.term), collapse="+")))
     fit2 <- glm(formula.term, D, family=family, na.action=na.omit, model=FALSE)
 
-    anova(fit, fit2, test="LRT")
+    t <- anova(fit, fit2, test="LRT")
+
+    list(test=t, coef=coefficients(summary(fit2)))
   }, mc.cores=num_threads)
   names(out) <- paste0(tests$term, ".", tests$child)
 
-  pvalues <- sapply(out, function(t) t$`Pr(>Chi)`[2])
-
   list(
-    tests=out,
-    pvalue=pvalues
+    coef=lapply(out, `[[`, "coef"),
+    test=lapply(out, `[[`, "test")
   )
 }
 
@@ -124,6 +134,7 @@ test_terms_children <- function(formula, covars, pc, o, family=binomial("logit")
 #' @param covars A data frame containing covariates.
 #' @param pc Named list of PCs from genes. Computed with \code{\link{compute_gene_pcs}}.
 #' @param family Error distribution and link function to be used in the model. See \code{\link{glm.fit}} for details.
+#' @param Test statistic for model comparison. One of "Chisq", "LRT", "Rao", "F" or "Cp". See \code{\link{stat.anova}}.
 #' @param max_pcs Maximum number of PCs to use per gene.
 #' @param num_threads Number of threads.
 #' @return A list with elements:
@@ -134,6 +145,6 @@ test_terms_children <- function(formula, covars, pc, o, family=binomial("logit")
 #'     \item{\code{pvalue}}{p-values for each gene}
 #'   }
 #' @export
-test_genes <- function(formula, covars, pc, family=binomial("logit"), max_pcs=Inf, num_threads=1) {
+test_genes <- function(formula, covars, pc, family=binomial("logit"), test=NULL, max_pcs=Inf, num_threads=1) {
   test_terms(formula, covars, pc, family=family, max_pcs=max_pcs, num_threads=num_threads)
 }
