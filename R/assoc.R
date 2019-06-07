bb46c45fc56fc2d52af32ced52eaad683d2385e1
Simon Larsen <simonhffh@gmail.com>
2019-06-07 15:22:44 +0200

    Changed test_terms output to DF using broom

--- R/assoc.R
+++ R/assoc.R
@@ -14,11 +14,13 @@
 #' @param max_pcs Maximum number of PCs to use per term.
 #' @return A list with elements:
 #'   \describe{
-#'     \item{\code{test}}{Test objects for each term.}
+#'     \item{\code{test}}{Data frame of test statistics.}
 #'     \item{\code{coef}}{Coefficients from each term model.}
 #'   }
 #' @importFrom future.apply future_lapply
 #' @importFrom fastmatch "%fin%"
+#' @importFrom broom tidy
+#' @importFrom dplyr bind_rows
 #' @export
 test_terms <- function(formula, covars, pc, family=binomial("logit"), test=NULL, terms=NULL, max_pcs=Inf) {
   if(class(formula) != "formula") stop("formula is not a formula.")
@@ -46,90 +48,18 @@ test_terms <- function(formula, covars, pc, family=binomial("logit"), test=NULL,
     formula.term <- update(formula, paste0("~ . + ", paste0(colnames(x), collapse="+")))
     fit <- glm(formula.term, data=D, family=family, na.action=na.omit, model=FALSE, x=FALSE, y=FALSE)
 
-    t <- anova(reference.model, fit, test="LRT")
-
     list(
-      test=t,
+      test=anova(reference.model, fit, test=test),
       coef=coefficients(summary(fit))
     )
   })
 
-  list(
-    coef=lapply(out, `[[`, "coef"),
-    test=lapply(out, `[[`, "test")
-  )
-}
-
-#' Check terms for significance against children.
-#'
-#' @param formula An object of class \code{formula}.
-#' @param covars A data frame containing covariates.
-#' @param pc Named list of PCs from terms. Computed with \code{\link{compute_term_pcs}}.
-#' @param o An \code{ontology} object.
-#' @param family Error distribution and link function to be used in the model. See \code{\link{glm.fit}} for details.
-#' @param Test statistic for model comparison. One of "Chisq", "LRT", "Rao", "F" or "Cp". See \code{\link{stat.anova}}.
-#' @param terms Character vector of terms to test. Defaults to all terms.
-#' @param max_pcs Maximum number of PCs to use per term.
-#' @return A list with elements:
-#'   \describe{
-#'     \item{\code{test}}{Test objects for each term.}
-#'     \item{\code{coef}}{Coefficients from each term model.}
-#'   }
-#' @importFrom fastmatch "%fin%"
-#' @importFrom future.apply future_lapply
-#' @export
-test_terms_children <- function(formula, covars, pc, o, family=binomial("logit"), test=NULL, terms=NULL, max_pcs=Inf) {
-  if(class(formula) != "formula") stop("formula is not a formula.")
-  if(!("data.frame" %in% class(covars))) stop("covars is not a data frame.")
-  if(any(grepl("TERM[0-9]+", colnames(covars)))) stop("Covariates named \"TERM[n]\" where [n] is a number are not allowed.")
-  if(any(grepl("CHILD[0-9]+", colnames(covars)))) stop("Covariates named \"CHILD[n]\" where [n] is a number are not allowed.")
-  if(any(sapply(pc, is.null))) stop("Principal component vector contains NULL values.")
-
-  if(is.null(test)) {
-    if(family$family %in% c("binomial","poisson")) test <- "Chisq"
-    else if(family$family %in% c("gaussian","quasibinomial","quasipoisson")) test <- "F"
-    else stop("Don't know which test statistic to use for family \"", family$family, "\"")
-  }
-  if(is.null(terms)) terms <- names(pc)
-  terms <- intersect(terms, go$id[lengths(go$children) > 0])
-
-  out <- future_lapply(setNames(terms, terms), function(term) {
-    x.term <- .pcs_from_term(pc[[term]])
-    if(ncol(x.term) > max_pcs) x.term <- x.term[,seq_len(max_pcs), drop=FALSE]
-    colnames(x.term) <- paste0("TERM", seq_len(ncol(x.term)))
-
-    x.child <- pc[go$children[[term]]]
-    x.child <- x.child[!sapply(x.child, is.null)]
-    x.child <- lapply(x.child, .pcs_from_term)
-    x.child <- lapply(x.child, function(y) {
-      if(ncol(y) > max_pcs) y <- y[,seq_len(max_pcs), drop=FALSE]
-      y
-    })
-    x.child <- do.call(cbind, x.child)
-
-    if(!is.null(x.child)) {
-      colnames(x.child) <- paste0("CHILD", seq_len(ncol(x.child)))
-      D <- cbind(covars, x.child, x.term, stringsAsFactors=FALSE, row.names=NULL)
-      formula.child <- update(formula, paste0("~ . + ", paste0(colnames(x.child), collapse="+")))
-    } else {
-      D <- cbind(covars, x.term, stringsAsFactors=FALSE, row.names=NULL)
-      formula.child <- formula
-    }
-
-    fit <- glm(formula.child, D, family=family, na.action=na.omit, model=FALSE)
-
-    formula.term <-  update(formula.child, paste0("~ . + ", paste0(colnames(x.term), collapse="+")))
-    fit2 <- glm(formula.term, D, family=family, na.action=na.omit, model=FALSE)
-
-    t <- anova(fit, fit2, test="LRT")
-
-    list(test=t, coef=coefficients(summary(fit2)))
-  })
-  names(out) <- paste0(tests$term, ".", tests$child)
+  tests <- lapply(out, `[[`, "test")
+  tests <- cbind(term=terms, bind_rows(lapply(tests, function(t) tidy(t)[2,])))
 
   list(
     coef=lapply(out, `[[`, "coef"),
-    test=lapply(out, `[[`, "test")
+    test=tests
   )
 }
 
@@ -139,16 +69,14 @@ test_terms_children <- function(formula, covars, pc, o, family=binomial("logit")
 #' @param covars A data frame containing covariates.
 #' @param pc Named list of PCs from genes. Computed with \code{\link{compute_gene_pcs}}.
 #' @param family Error distribution and link function to be used in the model. See \code{\link{glm.fit}} for details.
-#' @param Test statistic for model comparison. One of "Chisq", "LRT", "Rao", "F" or "Cp". See \code{\link{stat.anova}}.
+#' @param test Test statistic for model comparison. One of "Chisq", "LRT", "Rao", "F" or "Cp". See \code{\link{stat.anova}}.
 #' @param max_pcs Maximum number of PCs to use per gene.
 #' @return A list with elements:
 #'   \describe{
-#'     \item{\code{reference}}{Reference model.}
-#'     \item{\code{coef}}{Coefficients from each gene model.}
-#'     \item{\code{tests}}{Test objects for each gene}
-#'     \item{\code{pvalue}}{p-values for each gene}
+#'     \item{\code{test}}{Data frame of test statistics.}
+#'     \item{\code{coef}}{Coefficients from each term model.}
 #'   }
 #' @export
 test_genes <- function(formula, covars, pc, family=binomial("logit"), test=NULL, max_pcs=Inf) {
-  test_terms(formula, covars, pc, family=family, max_pcs=max_pcs)
+  test_terms(formula, covars, pc, family=family, test=test, max_pcs=max_pcs)
 }
